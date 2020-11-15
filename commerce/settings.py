b821ab30311b0d8e5e8788a1bd90643cace2bd08
Doug Mumme <dougmumme@gmail.com>
2020-11-15 16:24:48 -0600

    removed unnecessary code

--- commerce/settings.py
+++ commerce/settings.py
@@ -125,8 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
