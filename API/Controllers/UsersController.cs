dfd46bea18466d0a0913bb7357a616c86c17018a
Doug Mumme <dougmumme@gmail.com>
2021-05-30 17:29:06 -0500

    add dotnet app

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+using API.data;
+using API.entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController: ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<appUser>>> getUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<appUser>> getUsers(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            return user;
+        }
+    }
+}
---
+++ API/entities/appUser.cs
@@ -0,0 +1,10 @@
+
+namespace API.entities
+{
+    public class appUser
+    {
+        public int id { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
